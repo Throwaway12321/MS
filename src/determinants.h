a48e386290325684d79288371cecba3e96fab633
Kuaaro <120309479+Kuaaro@users.noreply.github.com>
2022-12-14 22:03:54 +0100

    v0.2
    Dodanie prototypu funkcji oraz  struktury listy double

--- src/determinants.h
+++ src/determinants.h
@@ -4,6 +4,15 @@
 #include <stdio.h>
 #include "mat_io.h"
 
+typedef struct{
+    double *list;
+    int size;
+}double_arr;
 
+void free_d_arr(double_arr *in); /*Zwalnia double_arr*/
+
+double_arr *get_determ(matrix *in); /*tworzy wyznaczniki macierzy*/
+
+int solve_determ(double_arr *in); /*Rozwiazuje rownanie z pomoca wyznacznikow oraz zwalnia wyznaczniki*/
 
 #endif
