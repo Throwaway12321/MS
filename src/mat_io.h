2d597d584cb30e17f99dde145038e6a6ff231f9e
Kuaaro <120309479+Kuaaro@users.noreply.github.com>
2022-12-14 21:50:43 +0100

    v0.1
    Pierwsza wersja pliku nagłówkowego mat_io.h

--- src/mat_io.h
+++ src/mat_io.h
@@ -1,1 +1,18 @@
+#ifndef _MAT_IO_H_
+#define _MAT_IO_H_
 
+#include <stdio.h>
+
+typedef struct{
+    double *list;
+    int m, n;
+} matrix;
+
+matrix *make_matrix(FILE *in); /*Tworzy matryce z pliku*/
+
+void free_matr(matrix *in);/*Zwalnia matryce*/
+
+void show_matr(matrix *in);/*Wyswietla elementy matrycy*/
+
+
+#endif
