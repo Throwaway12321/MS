a68072b552f986a7009a417e0866ca7b7d6de406
Throwaway12321 <120312911+Throwaway12321@users.noreply.github.com>
2022-12-14 21:53:06 +0100

    v0.1
    Stworzenie prototypu pliku mat_io.c

--- src/mat_io.c
+++ src/mat_io.c
@@ -1,1 +1,20 @@
+#include "mat_io.h"
 
+#include <stdio.h>
+#include <stdlib.h>
+
+void free_matr(matrix *in){
+    free(in->list);
+    free(in->solutions);
+    free(in);
+}
+
+void show_matr(matrix *in){
+    int i, j;
+    for(i=0; i<in->m; i++){
+        for(j=0; j<in->n; j++)
+            printf("%f ", in->list[i*in->m+j]);
+        printf("| %f\n", in->solutions[i]);
+    }
+    printf("\n");
+}
