fadfbf3b62072590dd1f5a6c6794cf3a822f7062
Throwaway12321 <120312911+Throwaway12321@users.noreply.github.com>
2022-12-14 22:18:09 +0100

    v0.1
    Pierwotna wersja pliku main z funkcja main

--- src/main.c
+++ src/main.c
@@ -3,3 +3,28 @@
 
 #include "determinants.h"
 #include "mat_io.h"
+
+
+int main(int argc, char **argv){
+    int i;
+    matrix *matr;
+    double_arr *arr;
+    FILE *f;
+
+    for(i=1; i<argc; i++){
+        f = fopen(argv[i], "r");
+        if(f!=NULL){
+            matr = make_matrix(f);
+            fclose(f);
+            if(matr!=NULL){
+                show_matr(matr);
+                arr = get_determ(matr);
+                if(arr!=NULL)
+                    solve_determ(arr);
+            }
+            free_matr(matr);
+        } else
+            printf("%s: %s nie istnieje\n\n", argv[0], argv[i]);
+    }
+    return 0;
+}
